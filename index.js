0c96e93d6559cccf39cf53969fe13a6251735c93
samwpr <swpr10x@gmail.com>
2022-09-16 17:43:22 +0800

    Finish Colour Flipper

--- index.js
+++ index.js
@@ -1,13 +1,27 @@
+let dict = {0:0, 1: 1, 2: 2, 3:3, 4:4, 5:5, 6:6, 7:7, 8:8, 9:9, 10: "a", 11: "b", 12: "c", 13:"d", 14: "e", 15: "f"}; 
+
+function hexC(){
+    let hex = 16; 
+    let r = Math.floor(Math.random() * 220);
+    let rInt = r/hex;
+    let rIntFloor = Math.floor(rInt);
+    let rIntRemainder = rInt - rIntFloor;
+    let rIntRemainderXST = rIntRemainder * 16;
+    let r_as_hex = String(dict[rIntFloor]) + String(dict[rIntRemainderXST]);
+    return String(r_as_hex)
+    console.log(r_as_hex);
+}
+
+hexR = ""
+hexG = ""
+hexB = ""
 
 document.getElementById("btn").onclick = function generate(){
-    document.getElementById("color-code").innerHTML = "lol";
+    hexR = hexC();
+    hexG = hexC();
+    hexB = hexC();
+    hexCode = "#"+hexR + hexG + hexB;
+    document.getElementById("color-code").innerHTML = hexCode;
+    document.body.style.backgroundColor = hexCode;
 };
 
-let R = Math.random() * 220;
-console.log(R);
-
-/*
-
-
-
-*/
--- test.js
+++ test.js
@@ -2,27 +2,28 @@
 
 let dict = {1: 1, 2: 2, 3:3, 4:4, 5:5, 6:6, 7:7, 8:8, 9:9, 10: "a", 11: "b", 12: "c", 13:"d", 14: "e", 15: "f"}; 
 
+hexR = ""
+hexG = ""
+hexB = ""
 
 function hexC(){
     let hex = 16; 
-
+    console.log(hex);
     let r = Math.floor(Math.random() * 220);
     console.log(r);
-
     let rInt = r/hex;
     console.log(rInt);
-
     let rIntFloor = Math.floor(rInt);
     console.log(rIntFloor);
-
     let rIntRemainder = rInt - rIntFloor;
     console.log(rIntRemainder);
-
     let rIntRemainderXST = rIntRemainder * 16;
     console.log(rIntRemainderXST);
-
-    let r_as_hex = dict[rIntFloor].concat(dict[rIntRemainderXST])
+    let r_as_hex = String(dict[rIntFloor]) + String(dict[rIntRemainderXST]);
+    return String(r_as_hex);
     console.log(r_as_hex);
 }
 
-console.log(hexC());
+hexR = hexC();
+
+console.log(hexR);
